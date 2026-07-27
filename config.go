ef6a5a8892e5da20867fc5a570b1e3ab3e911d41
Seva Zaikov <seva.zaikov@gmail.com>
2026-07-26 22:20:19 -0700

    add basic config setup

---
+++ config.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type config struct {
+	Version   int    `json:"version"`
+	AgentTool string `json:"agentTool"`
+}
+
+func (c config) save() error {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	configPath := filepath.Join(homeDir, ".llyr", "")
+
+	data, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(configPath, data, 0644)
+}
+
+func parseConfig(fileName string) (config, error) {
+	var cfg config
+	data, err := os.ReadFile(fileName)
+
+	if err != nil {
+		return cfg, err
+	}
+
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+func (c *config) setTool() {
+	// check for 3 agents, but the user can have other agents, so
+	// it is possible to set up a different CLI option.
+	claudeExists := binaryExists("claude")
+	codexExists := binaryExists("codex")
+	piExists := binaryExists("pi")
+
+	options := []string{}
+
+	if claudeExists {
+		options = append(options, "claude")
+	}
+
+	if codexExists {
+		options = append(options, "codex")
+	}
+
+	if piExists {
+		options = append(options, "pi")
+	}
+
+	options = append(options, "Write a CLI app manually")
+
+	option, err := selectOption(options)
+
+	if err != nil {
+		fmt.Println("Could not select anything", err)
+		os.Exit(1)
+	}
+
+	// last choice is always select your own
+	if option == len(options)-1 {
+		// Wait for the line input
+	} else {
+		c.AgentTool = getToolOptions(options[option])
+	}
+
+	c.save()
+}
+
+func getToolOptions(tool string) string {
+	if tool == "claude" {
+		return "claude -p"
+	}
+
+	if tool == "codex" {
+		return "codex exec"
+	}
+
+	if tool == "pi" {
+		return "pi -p"
+	}
+
+	return tool
+}
+
+func binaryExists(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
+}
